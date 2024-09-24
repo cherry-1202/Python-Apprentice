9f59d4eab67f201198ce2d5f4deac08e471a3dcf
cherry-1202 <hwameek@gmail.com>
2024-09-24 01:28:52 +0000

    on 10b_tashmeclick.py

--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -19,18 +19,29 @@ t = turtle.Turtle()
 
 import turtle
 
-t.penup()
-t.shape("turtle")
-t.color('blue')
+def set_background_image(window, image_name):
+    """Set the background image of the turtle window to the image with the given name."""
+
+    from pathlib import Path
+    from PIL import Image
 
 
-sides=6
-angle=360/sides
-for i in range(sides):
-    t.pendown()
-    t.forward(50)
-    t.left(angle)
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
 
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
 
+# Set up the screen
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(width=600, height=600)     # Set the size of the window
 
-turtle.exitonclick()   
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+screen = turtle.Screen()                # Get the screen that tina is on
+set_background_image(screen, "emoji.png") # Set the background image of the screen
+
+turtle.exitonclick()      
+
